e912d848387a8930d06e000b65e5142dcdc7c704
remel-crowe <remelscrowe@gmail.com>
2024-01-08 18:09:03 +0000

    Main functionality complete and styled
    Most major parts have been implemented,the game is playable and the grid
    can be resized at will.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+const DEFAULT_SIZE = 16;
+
 let gridContainer = document.getElementsByClassName("grid-container")[0];
 let sizeValue = document.getElementById("sizeValue");
 let sizeSlider = document.getElementById("sizeSlider");
 let colourButton = document.getElementById("colourButton");
 let rainbowButton = document.getElementById("rainbowButton");
-let colours = ["green", "red", "blue", "pink", "yellow", "orange", "black"];
+let eraserButton = document.getElementById("eraserButton");
+let clearButton = document.getElementById("clearButton");
+let colourMode;
+
+sizeSlider.addEventListener("input", handleChange);
+rainbowButton.addEventListener("click", () => {
+  colourMode = "rainbow";
+});
+clearButton.addEventListener("click", clearGrid);
 
 function handleChange() {
   let gridSize = sizeSlider.value;
@@ -11,28 +21,39 @@ function handleChange() {
   clearGrid();
   createGrid(gridSize);
 }
-
 function createGrid(num) {
   gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
   gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-  var gridSquare;
+  let gridSquare;
   for (let i = 0; i < num * num; i++) {
     gridSquare = document.createElement("div");
     gridSquare.className = "grid-square";
     gridContainer.appendChild(gridSquare);
   }
-}
-function clearGrid() {
-  while (gridContainer.lastElementChild) {
-    gridContainer.removeChild(gridContainer.lastElementChild);
-  }
+  gridContainer.removeEventListener("mouseover", changeColour);
+  gridContainer.addEventListener("mouseover", changeColour);
 }
 
-sizeSlider.addEventListener("change", handleChange);
-
-gridContainer.addEventListener("mouseover", (event) => {
+function changeColour(event) {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
   if (event.target.classList.contains("grid-square")) {
-    event.target.style.background =
-      colours[Math.floor(Math.random() * colours.length)];
+    if (colourMode === "rainbow") {
+      event.target.style.background = `rgb(${r}, ${g}, ${b})`;
+    } else if (colourMode === "eraser") {
+      event.target.style.background = "white";
+    } else {
+    }
   }
-});
+}
+
+function clearGrid() {
+  gridContainer.innerHTML = "";
+  createGrid(sizeSlider.value);
+}
+
+window.onload = () => {
+  createGrid(DEFAULT_SIZE);
+  colourMode = "rainbow";
+};
